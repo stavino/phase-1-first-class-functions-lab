c426ce39743d78d6c19aaa7dc0cf9b009c53f818
Stephen Avino <stephen.avino@gmail.com>
2021-11-03 12:33:02 -0400

    commit tree c3a7869485d1041314aa54fdb1f115cc535d6433

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+let firstTwo;
+const returnFirstTwoDrivers = function(drivers = []){
+    return firstTwo = [drivers[0], drivers[1]]
+}
+let LastTwo;
+const returnLastTwoDrivers = function(drivers = []){
+    return LastTwo = drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+let farePrice;
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare*multiplier}
+};
+function fareDoubler(fare){
+    const double = createFareMultiplier(2);
+    return double(fare);
+}
+function fareTripler(fare){
+    const triple = createFareMultiplier(3);
+    return triple(fare);
+}
+function selectDifferentDrivers(arrayOfDrivers, Function){
+    return Function(arrayOfDrivers);
+}
